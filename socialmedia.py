1dc87a81f2f1eeea83dfd611d38941a148a4d61e
MariaDelCielo <mmendez@ucvts.org>
2016-12-22 14:43:54 +0000

    Commit new file

---
+++ socialmedia.py
@@ -0,0 +1,10 @@
+#import requests
+import requests
+
+#create url for requests 
+#endpoint for forecast request
+endpoint = 'https://data.cityofnewyork.us/api/views/'
+
+#deal with information received
+media = r.json()
+
